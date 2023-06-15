90612adc0bf9df5b2b7d74c65d8d78c596670818
Rodymus <61990609+Rodymus@users.noreply.github.com>
2023-06-15 10:30:25 -0300

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 import ItemListContainer from './components/items/ItemListContainer';
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-
+import ItemDetailConainer from './components/items/ItemDetailContainer';
 
 const App = () => {
   return (
-  <>
-  <ChakraProvider> 
-  <BrowserRouter>
-      <Routes>
-        <Route element= { <Layout />} >
-        <Route path="/" element={<ItemListContainer greeting={"Bienvenidos a la tienda de Vinos!!!"} />} />
-        </Route>
-              
-      </Routes>
-    </BrowserRouter>
-     <ItemListContainer /> 
-  </ChakraProvider>
-  </>
+    <>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route
+                path="/"
+                element={
+                  <ItemListContainer
+                    greeting={'Bienvenidos a la tienda de Vinos!!!'}
+                  />
+                }
+              />
+              <Route path="/item/:id" element={<ItemDetailConainer />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <ItemListContainer />
+      </ChakraProvider>
+    </>
   );
 };
 
